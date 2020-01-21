ea114be077e538651a70f150da9b2eaaf496107b
EnTiia <porgand_@hotmail.com>
2020-01-21 22:49:34 +0200

    päivityksiä

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import lunchList from './assets/sodexo-day-example.json';
+
 window.addEventListener('DOMContentLoaded', (event) => {
   let language = 'fi';
   let sortDirection = 'asc';
@@ -7,24 +9,15 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   const randomCourse = document.createElement('p');
 
-  const coursesEn = ["Hamburger, cream sauce and poiled potates",
-    "Goan style fish curry and whole grain rice",
-    "Vegan Chili sin carne and whole grain rice",
-    "Broccoli puree soup, side salad with two napas",
-    "Lunch baguette with BBQ-turkey filling",
-    "Cheese / Chicken / Vege / Halloum burger and french fries"];
+  const lunches = Object.values(lunchList.courses);
 
-  const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä perunaa",
-    "Goalaista kalacurrya ja täysjyväriisiä",
-    "vegaani Chili sin carne ja täysjyväriisi",
-    "Parsakeittoa,lisäkesalaatti kahdella napaksella",
-    "Lunch baguette with BBQ-turkey filling",
-    "Juusto / Kana / Kasvis / Halloumi burgeri ja ranskalaiset"];
+  const coursesFi = lunches.map(a => a.title_fi);
+  const coursesEn = lunches.map(a => a.title_en);
 
   const printMenu = (list) => {
     const menu = document.querySelector('#lunchList');
     menu.innerHTML = '';
-    for (item of list) {
+    for (let item of list) {
       console.log(item);
       const menuItem = document.createElement('li');
       menuItem.innerText = item;
