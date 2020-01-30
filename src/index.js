5b4abee58de81d3bf5ded47e7f2801eb1b1dca53
EnTiia <porgand_@hotmail.com>
2020-01-30 02:27:19 +0200

    muutoksia

--- src/index.js
+++ src/index.js
@@ -1,37 +1,71 @@
-import {StartGame, checkGuess, resetGame} from './modules/guess-game';
+let allGuesses = 0;
 
-StartGame();
+// The guessing game uses the binary search to find the random number.
+const guessingGame = () => {
+  const minLimit = 1, maxLimit = 100;
+  const maxGuess = 20;
+  let guessedNumber = null;
+  let guesses = 0;
+  let randomNumber = Math.floor(Math.random() * maxLimit) + minLimit;
+  console.log('random number: ' + randomNumber);
+  let middle = maxLimit / 2;
+  let min = 1;
+  let max = 100;
+  let guessArray = [];
 
-// TODO: Develop better algorithm
+  while (guesses < maxGuess) {
+    // The first guess is set to be the middle value.
+    if (guessedNumber === null) {
+      guessedNumber = middle;
+    }
+    guesses++;
+    allGuesses++;
+    // Saving guessed numbers to an array.
+    guessArray.push(guessedNumber);
 
-const testGamePlay = () => {
-  let guessCounter = 0;
-  let myGuess = 50;
-  let gameOver = false;
-  while(!gameOver) {
-    let correctGuess = checkGuess(myGuess);
-    guessCounter++;
-    if (correctGuess === 0) {
-      gameOver = true;
-      resetGame();
-    } else if (correctGuess < 0){
-      myGuess++;
+    if (guessedNumber === randomNumber) {
+      console.log('Your guess was right: ' + guessedNumber);
+      break;
     } else {
-      myGuess--;
+      // If the guessed number is too low, min value will be changed to the guessed value.
+      // Middle value is changed by calculating the new middle value between max value and guessed number.
+      // max + 1 (so we can guess the number 100).
+      if (guessedNumber < randomNumber) {
+        console.log('Too low: ' + guessedNumber);
+        min = guessedNumber;
+        middle = guessedNumber + ((max + 1 - guessedNumber) / 2);
+        guessedNumber = Math.floor(middle);
+        continue;
+        // If the guessed number is too high, max value will be changed to the guessed value.
+        // Middle value is changed by calculating the new middle value between guessed number and min value.
+      } else if (guessedNumber > randomNumber) {
+        console.log('Too high: ' + guessedNumber);
+        max = guessedNumber;
+        middle = guessedNumber - ((guessedNumber - min) / 2);
+        guessedNumber = Math.floor(middle);
+        continue;
+      }
     }
   }
-  return guessCounter;
+  // Displays the guessed numbers.
+  document.querySelector('.guesses').textContent = ('Guessed numbers: ' + guessArray);
+  document.querySelector('.results').textContent = ('Number of guesses: ' + guesses);
+
+  return guesses;
 };
 
-//testGamePlay();
-
 let guessCounts = [];
-for(let i=0; i<1000; i++) {
-  guessCounts.push(testGamePlay());
+for (let i = 0; i < 1000; i++) {
+  guessCounts.push(guessingGame());
 }
 console.log('guess counts', guessCounts);
 
 let maxGuessCount = Math.max(...guessCounts);
-console.log(maxGuessCount);
+console.log('Max guess count: ' + maxGuessCount);
+console.log('The theoretical maximum guesses with binary search from numbers between 1 and 100 is 7.');
 
-// TODO: Display max & average of guess counts
+let minGuessCount = Math.min(...guessCounts);
+console.log('Min guess count: ' + minGuessCount);
+
+let avgGuessCount = guessCounts.reduce((a, b) => a + b, 0) / guessCounts.length;
+console.log('Average guess count: ' + avgGuessCount);
