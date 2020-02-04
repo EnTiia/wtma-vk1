6d676cfd97bb46055ba33cb73c7d6fa6c601c4ce
EnTiia <porgand_@hotmail.com>
2020-02-04 22:41:41 +0200

    muutoksia

--- src/index.js
+++ src/index.js
@@ -1,124 +1,58 @@
-import lunchList from './assets/sodexo-day-example.json';
+const cheatList = ['h', 'e', 'l', 'l', 'o'];
+let keyNumber = 0;
 
-window.addEventListener('DOMContentLoaded', (event) => {
-  let language = 'fi';
-  let sortDirection = 'asc';
-
-  const cardContent = document.querySelector('div.card-content');
-  const main = document.querySelector('main');
-
-  const randomCourse = document.createElement('p');
-
-  const lunches = Object.values(lunchList.courses);
-
-  const coursesFi = lunches.map(a => a.title_fi);
-  const coursesEn = lunches.map(a => a.title_en);
-
-  const printMenu = (list) => {
-    const menu = document.querySelector('#lunchList');
-    menu.innerHTML = '';
-    for (let item of list) {
-      console.log(item);
-      const menuItem = document.createElement('li');
-      menuItem.innerText = item;
-      menu.appendChild(menuItem);
-    }
-  };
-
-  const sortArray = (array, sort) => {
-    const sortAsc = (a, b) => {
-      if (a > b) {
-        return 1;
-      } else if (b > a) {
-        return -1;
-      } else {
-        return 0;
-      }
-    };
-
-    const sortDesc = (a, b) => {
-      if (a < b) {
-        return 1;
-      } else if (b < a) {
-        return -1;
-      } else {
-        return 0;
-      }
-    };
-
-    if (sort === 'desc') {
-      return array.sort(sortDesc);
-    } else {
-      return array.sort(sortAsc);
-    }
-  };
-
-  const sortButton = document.createElement('button');
-  sortButton.innerText = 'Järjestä';
-
-  sortButton.addEventListener('click', event => {
-    const array = (language === 'fi') ? coursesFi : coursesEn;
-    const sortedArray = sortArray(array, sortDirection);
-    if (sortDirection === 'asc'){
-      sortDirection = 'desc';
-    } else {
-      sortDirection = 'asc';
-    }
-
-    printMenu(sortedArray);
-  });
-
-  const languageButton = document.createElement('button');
-  languageButton.innerText = 'Vaihda kieltä';
-
-  const randomButton = document.createElement('button');
-  randomButton.innerText = 'Arvo satunnainen ruokalaji';
-
-  languageButton.addEventListener('click', event => {
-    if (language === 'fi'){
-      language = 'en';
-      languageButton.innerText = 'Change language';
-      sortButton.innerText = 'Sort';
-      randomButton.innerText = 'Select random course';
-      printMenu(coursesEn);
-    } else {
-      language = 'fi';
-      languageButton.innerText = 'Vaihda kieltä';
-      sortButton.innerText = 'Järjestä';
-      randomButton.innerText = 'Arvo satunnainen ruokalaji';
-      printMenu(coursesFi);
-    }
-  });
-
-  randomButton.addEventListener('click', event => {
-    const array = (language === 'fi') ? coursesFi : coursesEn;
-    const random = Math.floor(Math.random()*array.length);
-    console.log(random);
-    randomCourse.innerText = array[random];
-  });
-
-  cardContent.appendChild(sortButton);
-  cardContent.appendChild(languageButton);
-  cardContent.appendChild(randomButton);
-  main.appendChild(randomCourse);
-
-  printMenu((language === 'fi') ? coursesFi : coursesEn);
+document.addEventListener('keydown', event => {
+  console.log(event.key);
+  if (event.key === cheatList[keyNumber]) {
+    keyNumber++;
+  } else {
+    keyNumber = 0;
+  }
+  if (keyNumber === 5) {
+    keyNumber = 0;
+    alert('Hello!');
+  }
 });
 
-const modal = document.getElementById('modal');
-const btn = document.getElementsByClassName('card-logo')[0];
-const span = document.getElementsByClassName('close-modal')[0];
+document.addEventListener('dblclick', event => {
+  console.log('X coords: ' + event.clientX + ', Y coords: ' + event.clientY);
+});
 
-btn.onclick = function() {
-  modal.style.display = "block";
-};
+const touchElement = document.createElement('div');
+const body = document.querySelector('body');
+body.appendChild(touchElement);
 
-span.onclick = function() {
-  modal.style.display = "none";
-};
+touchElement.addEventListener('touchstart', event => {
+  console.log('You touched the box!');
+});
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+const timeout = document.createElement('p');
+body.appendChild(timeout);
+window.addEventListener('DOMContentLoaded', event => {
+  setTimeout(() => timeout.innerText = 'Hurry!', 15000);
+});
+
+const idleTime = document.createElement('p');
+body.appendChild(idleTime);
+window.addEventListener('DOMContentLoaded', event => {
+  let idle = 0;
+
+  const timer = () => {
+    idle ++;
+    if (idle === 15) {
+      idleTime.innerText = 'Hurry up!';
+      idle = 0;
+    }
+  };
+
+  const reset = () => {
+    idle = 0;
+  };
+
+  setInterval(timer, 1000);
+
+  window.addEventListener('mousedown', reset);
+  window.addEventListener('mousemove', reset);
+  window.addEventListener('keyup', reset);
+  window.addEventListener('keydown', reset);
+});
