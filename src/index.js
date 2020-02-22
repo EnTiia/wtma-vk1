a376597e34ffc49e9761ce06c1bde276505c5d4d
EnTiia <porgand_@hotmail.com>
2020-02-22 21:06:15 +0200

    muutoksia

--- src/index.js
+++ src/index.js
@@ -1,111 +1,36 @@
 import lunchList from './assets/sodexo-day-example.json';
 
 window.addEventListener('DOMContentLoaded', (event) => {
-  let language = 'fi';
-  let sortDirection = 'asc';
-
-  const menuButtons = document.querySelector('div.menu-buttons');
-  const main = document.querySelector('main');
-
-  const randomCourse = document.createElement('p');
 
   const lunches = Object.values(lunchList.courses);
 
   const coursesFi = lunches.map(a => a.title_fi);
-  const coursesEn = lunches.map(a => a.title_en);
+  const categories = lunches.map(a => a.category);
 
-  const printMenu = (list) => {
+  const printMenu = (list, food) => {
     const menu = document.querySelector('#lunchList');
+    const modalBody = document.querySelector('.modal-body');
     menu.innerHTML = '';
     for (let item of list) {
       console.log(item);
       const menuItem = document.createElement('li');
       menuItem.innerText = item;
+      const modalText = document.createElement('li');
+      modalText.innerText = item;
       menu.appendChild(menuItem);
+      modalBody.appendChild(modalText);
+    }
+    for (let item of food) {
+      console.log(item);
+      const menuItem = document.createElement('li');
+      menuItem.innerText = item;
+      const modalText = document.createElement('li');
+      modalText.innerText = item;
+      menu.appendChild(menuItem);
+      modalBody.appendChild(modalText);
     }
   };
-
-  const sortArray = (array, sort) => {
-    const sortAsc = (a, b) => {
-      if (a > b) {
-        return 1;
-      } else if (b > a) {
-        return -1;
-      } else {
-        return 0;
-      }
-    };
-
-    const sortDesc = (a, b) => {
-      if (a < b) {
-        return 1;
-      } else if (b < a) {
-        return -1;
-      } else {
-        return 0;
-      }
-    };
-
-    if (sort === 'desc') {
-      return array.sort(sortDesc);
-    } else {
-      return array.sort(sortAsc);
-    }
-  };
-
-  const sortButton = document.createElement('button');
-  sortButton.classList.add('sort-button');
-  sortButton.innerText = 'Järjestä';
-
-  sortButton.addEventListener('click', event => {
-    const array = (language === 'fi') ? coursesFi : coursesEn;
-    const sortedArray = sortArray(array, sortDirection);
-    if (sortDirection === 'asc'){
-      sortDirection = 'desc';
-    } else {
-      sortDirection = 'asc';
-    }
-
-    printMenu(sortedArray);
-  });
-
-  const languageButton = document.createElement('button');
-  languageButton.classList.add('language-button');
-  languageButton.innerText = 'Vaihda kieltä';
-
-  const randomButton = document.createElement('button');
-  randomButton.classList.add('random-button');
-  randomButton.innerText = 'Arvo satunnainen ruokalaji';
-
-  languageButton.addEventListener('click', event => {
-    if (language === 'fi'){
-      language = 'en';
-      languageButton.innerText = 'Change language';
-      sortButton.innerText = 'Sort';
-      randomButton.innerText = 'Select random course';
-      printMenu(coursesEn);
-    } else {
-      language = 'fi';
-      languageButton.innerText = 'Vaihda kieltä';
-      sortButton.innerText = 'Järjestä';
-      randomButton.innerText = 'Arvo satunnainen ruokalaji';
-      printMenu(coursesFi);
-    }
-  });
-
-  randomButton.addEventListener('click', event => {
-    const array = (language === 'fi') ? coursesFi : coursesEn;
-    const random = Math.floor(Math.random()*array.length);
-    console.log(random);
-    randomCourse.innerText = array[random];
-  });
-
-  menuButtons.appendChild(sortButton);
-  menuButtons.appendChild(languageButton);
-  menuButtons.appendChild(randomButton);
-  main.appendChild(randomCourse);
-
-  printMenu((language === 'fi') ? coursesFi : coursesEn);
+  printMenu(coursesFi, categories);
 });
 
 const modal = document.getElementById('modal');
@@ -125,3 +50,51 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 };
+
+
+//Media rules
+const logoText = document.createElement('div');
+logoText.innerText = 'Lounari.';
+logoText.id = 'logo-text';
+const slogan = document.createElement('div');
+slogan.innerText = 'Missä tänään syötäisiin.';
+slogan.id = 'slogan';
+const imgContainer = document.createElement('div');
+imgContainer.classList.add("img-container");
+
+const headerMedia = (media) => {
+  const headerImg = document.querySelector('#header-img');
+  const headerContent = document.querySelector('.header-content');
+  if (media.matches) { // If media query matches
+    headerImg.classList.remove("polygon");
+    headerImg.appendChild(logoText);
+    headerImg.appendChild(slogan);
+    headerImg.appendChild(imgContainer);
+
+  } else {
+    headerImg.classList.add("polygon");
+    headerContent.appendChild(logoText);
+    headerContent.appendChild(slogan);
+  }
+};
+
+
+let media = window.matchMedia("(max-width: 700px)");
+headerMedia(media);// Call listener function at run time
+media.addListener(headerMedia); // Attach listener function on state changes
+
+//Hamburger menu
+const mobileNavBtn = document.querySelector('#mobile-nav-btn');
+const navContent = document.querySelector('.nav-content');
+const bars = document.querySelector('.fa-bars');
+const nav = document.querySelector('nav');
+
+mobileNavBtn.addEventListener('click', event => {
+  if (bars.className === 'fas fa-bars') {
+    bars.classList.toggle('fa-times');
+    nav.style.height = '100%';
+  } else {
+    bars.className = ('fas fa-bars');
+    nav.style.height = '1rem';
+  }
+});
--- src/nav.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener('DOMContentLoaded', event => {
-  const navButton = document.querySelector('#nav-button');
-});
