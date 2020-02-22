5654e81a26eea175bf798192333d75cdab3efbf6
EnTiia <porgand_@hotmail.com>
2020-02-22 22:26:44 +0200

    muutoksia

--- src/index.js
+++ src/index.js
@@ -1,51 +1,53 @@
+//Getting the lunc lists
 import lunchList from './assets/sodexo-day-example.json';
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
-  const lunches = Object.values(lunchList.courses);
+  let coursesFi = [];
 
-  const coursesFi = lunches.map(a => a.title_fi);
-  const categories = lunches.map(a => a.category);
-
-  const printMenu = (list, food) => {
-    const menu = document.querySelector('#lunchList');
-    const modalBody = document.querySelector('.modal-body');
-    menu.innerHTML = '';
-    for (let item of list) {
-      console.log(item);
-      const menuItem = document.createElement('li');
-      menuItem.innerText = item;
-      const modalText = document.createElement('li');
-      modalText.innerText = item;
-      menu.appendChild(menuItem);
-      modalBody.appendChild(modalText);
+  const parseSodexoMenu = (sodexoDailyMenu) => {
+    const courses = Object.values(sodexoDailyMenu);
+    for (const course of courses) {
+      coursesFi.push(course.category + ": " + course.title_fi);
     }
-    for (let item of food) {
-      console.log(item);
-      const menuItem = document.createElement('li');
-      menuItem.innerText = item;
+  };
+
+  parseSodexoMenu(lunchList.courses);
+
+  const SodexoData = { coursesFi };
+
+  const renderMenu = (menu) => {
+    const list = document.querySelector('#lunch-list');
+    const modalBody = document.querySelector('.modal-body');
+    list.innerHTML = '';
+    for (const item of menu) {
+      const listItem = document.createElement('li');
+      listItem.textContent = item;
       const modalText = document.createElement('li');
       modalText.innerText = item;
-      menu.appendChild(menuItem);
+      list.appendChild(listItem);
       modalBody.appendChild(modalText);
     }
   };
-  printMenu(coursesFi, categories);
+
+  renderMenu(SodexoData.coursesFi);
 });
 
+
+//Modal
 const modal = document.getElementById('modal');
 const btn = document.getElementsByClassName('card-logo')[0];
 const span = document.getElementsByClassName('close-modal')[0];
 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 };
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
@@ -82,6 +84,7 @@ const headerMedia = (media) => {
 let media = window.matchMedia("(max-width: 700px)");
 headerMedia(media);// Call listener function at run time
 media.addListener(headerMedia); // Attach listener function on state changes
+
 
 //Hamburger menu
 const mobileNavBtn = document.querySelector('#mobile-nav-btn');
